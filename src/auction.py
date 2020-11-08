928ea983cc655c47bd08ada69e8130ed2b40a215
Denis Verstraeten <denis.verstraeten@ulb.ac.be>
2020-11-08 18:57:16 +0100

    creation of bidders working

--- src/auction.py
+++ src/auction.py
@@ -164,194 +164,19 @@ class Auction:
             logging.info('Deploying smart contract.')
             self.deploy()
 
-        # Registering filters
-        z_i_filter = self.__contract.events.submittedZ_i.createFilter(fromBlock='latest')
-        c_i1_filter = self.__contract.events.submittedC_i1.createFilter(fromBlock='latest')
-
         # Generating auctioneer and bidders
         self.__auctioneer = Auctioneer(address=self.__w3.eth.defaultAccount)
         logging.info(f'Auctioneer created: {self.__auctioneer}.')
         self.__bidders = get_bidders(Path('bidders.json'))  # If new file is created,
         # max number of bidders must be < number of accounts on the blockchain.
-
         bidder_addresses = sample(self.__w3.eth.accounts[1:], len(self.__bidders))
         # Randomly picks n = len(self.__bidders) addresses out of the accounts list.
         # Element zero is excluded because it is auctioneer address.
-
         for (index, bidder) in enumerate(self.__bidders):
-
             bidder.address = bidder_addresses[index]
             bidder.auctioneer_pub_key = self.__auctioneer.public_key
 
         logging.debug(f'Bidders created: {self.__bidders}.')
-
-        # Generating ring of public keys
-        keys = [self.__auctioneer.public_key]
-        for bidder in self.__bidders:
-
-            keys.append(bidder.public_key)
-
-        shuffle(keys)
-        self.__auctioneer.set_ring(keys)
-        for bidder in self.__bidders:
-
-            bidder.set_ring(keys)
-
-        # Generating and sending bidder's commitments
-        data_lengths = {}  # Will be used later to help parsing concatenated messages. Not very elegant.
-        for bidder in self.__bidders:
-
-            logging.info(f'Generating and sending messages for bidder {bidder.name} at {bidder.address}.')
-            bidder.generate_commitments()
-            data_lengths[bidder.address] = {
-                'r_i': len(bidder.r_i),
-                'c_i': len(bidder.c_i),
-                'sigma_i': len(bidder.sigma_i),
-                'Sigma_i': len(bidder.Sigma_i),
-                'bid': int(256 / 8),
-                'delta_i': len(bidder.delta_i),
-                'PK_bidder': len(bidder.public_key.export_key()),
-                'PK_auctioneer': len(bidder.auctioneer_pub_key.export_key())
-            }
-            tx = {
-                'from': bidder.address,
-                'to': self.__contract.address
-            }
-            self.__send_transaction(tx,
-                                    'submitZ_i',
-                                    bidder.c_i.hex(),
-                                    bidder.sigma_i.hex(),
-                                    bidder.com_i1.hex(),
-                                    bidder.com_i2.hex())
-
-        # Auctioneer storing commitments
-        for event in z_i_filter.get_new_entries():
-
-            bidder_address = event['args']['bidderAddress']
-            event_name = event['event']
-            logging.info(f'Catching event {event_name} from bidder at {bidder_address}.')
-            c_i = event['args']['c_i']
-            sigma_i = event['args']['sigma_i']
-            if self.__auctioneer.verify(c_i, sigma_i):
-
-                logging.info('Valid signature.')
-                bidder = Bidder(address=bidder_address,
-                                generate_new_keys=False)
-                bidder.c_i = c_i
-                bidder.sigma_i = sigma_i
-                bidder.com_i1 = event['args']['com_i1']
-                bidder.com_i2 = event['args']['com_i2']
-                self.__auctioneer.bidders[bidder_address] = bidder
-
-                logging.info(f'Storing Z_i for bidder at {bidder_address}.')
-                tx = {
-                    'from': self.__auctioneer.address,
-                    'to': self.__contract.address
-                }
-                self.__send_transaction(tx,
-                                        'storeZ_i',
-                                        bidder_address,
-                                        bidder.c_i.hex(),
-                                        bidder.sigma_i.hex(),
-                                        bidder.com_i1.hex(),
-                                        bidder.com_i2.hex())
-
-            else:
-
-                logging.info(f'Invalid signature for bidder at {bidder_address}.')
-                self.__black_list_bidder(bidder_address)
-
-        # Sending C_i1
-        for bidder in self.__bidders:
-
-            logging.info(f'Sending C_i1 for bidder {bidder.name} at {bidder.address}.')
-            tx = {
-                'from': bidder.address,
-                'to': self.__contract.address
-            }
-            self.__send_transaction(tx,
-                                    'submitC_i1',
-                                    bidder.C_i1.hex(),
-                                    int.from_bytes(bidder.r_i1, byteorder))
-
-        # Getting C_i1 and finding out winning bidder
-        winning_bid = 0
-        winning_bidder_address = None
-        for event in c_i1_filter.get_new_entries():
-
-            bidder_address = event['args']['bidderAddress']
-            event_name = event['event']
-            logging.info(f'Catching event {event_name} from bidder at {bidder_address}.')
-            if bidder_address in self.__auctioneer.bidders.keys():
-
-                c_i1 = event['args']['C_i1']
-                r_i1 = event['args']['r_i1'].to_bytes(int(256 / 8), byteorder)
-                if commit(c_i1, r_i1) == self.__auctioneer.bidders[bidder_address].com_i1:
-
-                    logging.info('C_i1 and r_i1 match commitment com_i1.')
-                    m_i1 = self.__auctioneer.decrypt(c_i1)
-
-                    # Parsing data in m_i1
-                    length = data_lengths[bidder_address]
-                    c_i = m_i1[:length['c_i']]
-                    sigma_i = m_i1[length['c_i']: length['c_i'] + length['sigma_i']]
-                    Sigma_i = m_i1[length['c_i'] + length['sigma_i']:
-                                   length['c_i'] + length['sigma_i'] + length['Sigma_i']]
-                    x_i = m_i1[length['c_i'] + length['sigma_i'] + length['Sigma_i']:
-                               length['c_i'] + length['sigma_i'] + length['Sigma_i'] + length['bid']]
-                    r_i = m_i1[length['c_i'] + length['sigma_i'] + length['Sigma_i'] + length['bid']:
-                               length['c_i'] + length['sigma_i'] + length['Sigma_i'] + length['bid'] + length['r_i']]
-
-                    if c_i == self.__auctioneer.bidders[bidder_address].c_i and commit(x_i, r_i) == c_i:
-
-                        logging.info('bid and r_i match commitment c_i.')
-                        if sigma_i == self.__auctioneer.bidders[bidder_address].sigma_i and \
-                                self.__auctioneer.verify(c_i, sigma_i):
-
-                            logging.info('Valid signature sigma_i on c_i.')
-                            if self.__auctioneer.verify(c_i + sigma_i, Sigma_i):
-
-                                logging.info('Valid signature Sigma_i on c_i||sigma_i.')
-                                if x_i > winning_bid:
-
-                                    logging.info('New winning bid.')
-                                    winning_bid = x_i
-                                    winning_bidder_address = bidder_address
-
-                                logging.info(f'Storing C_i1 and r_i1 for bidder at {bidder_address}.')
-                                tx = {
-                                    'from': self.__auctioneer.address,
-                                    'to': self.__contract.address
-                                }
-                                self.__send_transaction(tx,
-                                                        'storeC_i1',
-                                                        c_i1.hex(),
-                                                        int.from_bytes(r_i1, byteorder))
-
-                            else:
-
-                                logging.info('Invalid signature Sigma_i on c_i||sigma_i.')
-                                self.__black_list_bidder(bidder_address)
-
-                        else:
-
-                            logging.info('Invalid signature sigma_i on c_i.')
-                            self.__black_list_bidder(bidder_address)
-
-                    else:
-
-                        logging.info('bid and r_i do not match commitment c_i.')
-                        self.__black_list_bidder(bidder_address)
-
-                else:
-
-                    logging.info('C_i1 and r_i1 do not match com_i1.')
-                    self.__black_list_bidder(bidder_address)
-
-            else:
-
-                logging.info(f'Bidder at {bidder_address} not among registered bidders.')
-                self.__black_list_bidder(bidder_address)
 
     def __send_transaction(self,
                            transaction,
@@ -388,21 +213,3 @@ class Auction:
         logging.info(f'Gas used for transaction {tx_hash.hex()}: {gas_used} gas.')
         self.__total_gas_cost += gas_used
         logging.info(f'Total gas cost: {self.__total_gas_cost} gas.')
-
-    def __black_list_bidder(self,
-                            bidder_address: str
-                            ) -> None:
-        """
-        Black list bidder by calling the smart contract function.
-        :param bidder_address: Address of the bidder to be black listed.
-        """
-
-        logging.info(f'Black listing bidder at {bidder_address}.')
-        tx = {
-            'from': self.__auctioneer.address,
-            'to': self.__contract.address
-        }
-        self.__send_transaction(tx,
-                                'blackListBidder',
-                                bidder_address)
-        logging.info(f'Bidder at {bidder_address} black listed.')
